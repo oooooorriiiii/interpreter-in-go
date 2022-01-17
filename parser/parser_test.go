8b41187220e6eebd969e8ab5e1e51883002e40c5
Yudai Mori <yuu.mori06@gmail.com>
2022-01-17 15:34:44 +0900

    add: Parser Errors

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -10,12 +10,13 @@ func TestLetStatements(t *testing.T) {
 	input := `
 let x = 5;
 let y = 10;
-let foobar = 4242;
+let 4242;
 `
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -37,6 +38,19 @@ let foobar = 4242;
 			return
 		}
 	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
